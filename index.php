8083d9017a662ce697d1ee33637e3a03827bdc55
brian_moon <apollomoon@knights.ucf.edu>
2021-08-18 19:39:20 -0400

    added username field for registering, displayed contents of database on screen

--- index.php
+++ index.php
@@ -11,14 +11,58 @@
     <title>Lamp Stack CRUD</title>
   </head>
   <body>
-    <?php require_once 'processing.php'; ?>
+    <div style="width: 50%; padding-top: 5%;" class="container">
+      <?php require_once 'processing.php'; ?>
+
+      <?php
+        $mysqli = mysqli_connect('localhost', 'root', 'root', 'lamplogin') or die(mysqli_connect_error($mysqli));
+        $result = mysqli_query($mysqli, "SELECT * FROM data") or die($mysqli->error);
+      ?>
+
+      <div class="row justify-content-center">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Username</th>
+              <th colspan="2">Manage</th>
+            </tr>
+          </thead>
+      <?php
+        // db results
+        // pretty($result);
+        // db assoc array - looping to get all db entries
+        while($row = $result->fetch_assoc()): 
+      ?>
+          <tr>
+            <td><?php echo $row['email']; ?></td>
+            <td><?php echo $row['username']; ?></td>
+          </tr>
+        <?php endwhile; ?>
+        </table>
+      </div>
+      <?php
+        // make output pretty
+        function pretty($array) {
+          echo '<pre>';
+          print_r($array);
+          echo '</pre>';
+        }
+      ?>
+    </div>
+
     <div style="width: 50%; padding-top: 20%;" class="container">
-      <form action="processing.php" method="POST">
+      <form action="" method="POST">
         <div class="mb-3">
-          <h1>LAMP Stack Login Form</h1>
+          <h1>LAMP Stack Register Form</h1>
           <label for="email" class="form-label">Email address</label>
           <input name="email" type="email" class="form-control" aria-describedby="emailHelp">
           <div class="form-text">Please enter valid email address</div>
+        </div>
+        <div class="mb-3">
+          <label for="username" class="form-label">Username</label>
+          <input name="username" type="text" class="form-control" aria-describedby="emailHelp">
+          <div class="form-text">4-100 characters</div>
         </div>
         <div class="mb-3">
           <label for="password" class="form-label">Password</label>
--- processing.php
+++ processing.php
@@ -6,6 +6,9 @@ $mysqli = mysqli_connect('localhost', 'root', 'root', 'lamplogin') or die(mysqli
 if (isset($_POST['submit'])){
     $email = $_POST['email'];
     $pass = $_POST['password'];
+    $un = $_POST['username'];
 
-    $mysqli->query("INSERT INTO data (email, pwd) VALUES ('$email', '$pass')") or die($mysqli->error);
+    mysqli_query($mysqli, "INSERT INTO data (email, pwd, username) VALUES ('$email', '$pass', '$un')") or die($mysqli->error);
 }
+
+?>
